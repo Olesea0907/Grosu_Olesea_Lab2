374ffdeeee71dd234f4ca9b61ce24c931d0f5e50
Olesea0907 <olesea.grosu@stud.ubbcluj.ro>
2024-11-16 07:22:20 +0200

    .

---
+++ Models/BookCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace Grosu_Olesea_Lab2.Models
+{
+    public class BookCategory
+    {
+        public int ID { get; set; }
+        public int BookID { get; set; }
+        public Book Book { get; set; }
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+    }
+}
